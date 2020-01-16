50ae87592cd28f6b73466a5c37de0fb488a05368
Darcy LF <ehnevault@gmail.com>
2020-01-16 22:08:04 +1100

    v3 done, i think...

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,10 @@
 module.exports = function(eleventyConfig) {
     //sass('./style/index.scss', './_site/style/index.css');
     eleventyConfig.addPassthroughCopy("index.css");
-    
+    eleventyConfig.addPassthroughCopy("things.css");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("sitemap.xml");
+    eleventyConfig.addPassthroughCopy("icons");
     return {
         dir: {
           output: "docs"
