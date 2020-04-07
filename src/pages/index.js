1da07d1edb586389d6afbf437671f0eec7deff76
Darcy LF <ehnevault@gmail.com>
2020-04-07 16:51:41 +1000

    init gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <SEO title="darcy has the best website"></SEO>
+    <h1>Hi! My name's Darcy Lugt-Falk</h1>
+  </>
+)
+
+export default IndexPage
