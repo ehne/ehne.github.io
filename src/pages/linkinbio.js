3e9b8e0430f9e6688fd7de11817d41e4e74791a4
Darcy LF <ehnevault@gmail.com>
2020-04-09 15:45:31 +1000

    fix headroom issue

--- src/pages/linkinbio.js
+++ src/pages/linkinbio.js
@@ -4,7 +4,11 @@ import linksData from "../_data/links.json"
 import Project from "../components/project"
 import {Box} from "rebass"
 import { navigate } from 'gatsby';
+import styled from "@emotion/styled"
 
+const FontSize = styled.div`
+  font-size: 0.7em;
+`
 const ListOfLinks = () =>{
     if (linksData.length === 1){
         navigate("/")
@@ -27,13 +31,17 @@ const linkinbio = () => {
             title:"contact",
             to:"/contact"
         }}>
-            <Box fontSize="0.7em" marginTop="2em" sx={{
+            <FontSize>
+            <Box sx={{
                 maxWidth: "720px",
                 mx: "auto",
-                padding:"0.5em"
+                padding:"0.5em",
+                marginTop:"5em"
             }}>
                 <ListOfLinks></ListOfLinks>
             </Box>
+            </FontSize>
+            
             
         </Layout>
     );
