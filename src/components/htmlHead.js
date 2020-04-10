68592f5c639ff2c8348abaffeba2cff3fb644262
Darcy LF <ehnevault@gmail.com>
2020-04-10 11:46:16 +1000

    put the hook in the right place

--- src/components/htmlHead.js
+++ src/components/htmlHead.js
@@ -2,35 +2,41 @@ import React from "react"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const { site } = useStaticQuery(
-  graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-          description
-          author
+
+
+const Head = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
         }
       }
-    }
-  `
-)
-const metaDescription = site.siteMetadata.description
-
-const Head = () => (
+    `
+  )
+  const metaDescription = site.siteMetadata.description
+  
+  
+  return (
   <>
-    <Helmet title="darcy has the best website">
-    <title>darcy has the best website</title>
-    <link
-      href="https://cdn.jsdelivr.net/gh/sharanda/manrope@1a6035ff9cc9ce819ae2982dfa4cceb040aad77a/fonts/web/index.css"
-      rel="stylesheet"
-    />
+      <Helmet title="darcy has the best website">
+      <title>darcy has the best website</title>
+      <link
+        href="https://cdn.jsdelivr.net/gh/sharanda/manrope@1a6035ff9cc9ce819ae2982dfa4cceb040aad77a/fonts/web/index.css"
+        rel="stylesheet"
+      />
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
-    <meta name="description" content={metaDescription}/>
-    </Helmet>
-    
-  </>
-)
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
+      <meta name="description" content={metaDescription}/>
+      </Helmet>
+      
+    </>
+  )
+  
+}
 
 export default Head
