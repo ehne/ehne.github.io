86c0d7868b20fd87a494d77ae203fb6a8a0cd7bd
Darcy LF <ehnevault@gmail.com>
2020-04-11 16:42:59 +1000

    add og-images

--- src/components/htmlHead.js
+++ src/components/htmlHead.js
@@ -32,6 +32,18 @@ const Head = () => {
 
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
       <meta name="description" content={metaDescription}/>
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://darcylf.me/"/>
+      <meta property="og:title" content="darcy has the best website"/>
+      <meta property="og:description" content="Hi! I'm Darcy Lugt-Falk, a web designer and developer based in Melbourne"/>
+      <meta property="og:image" content="https://og.darcylf.me/image.js"/>
+
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content="https://darcylf.me/"/>
+      <meta property="twitter:title" content="darcy has the best website"/>
+      <meta property="twitter:description" content="Hi! I'm Darcy Lugt-Falk, a web designer and developer based in Melbourne"/>
+      <meta property="twitter:image" content="https://og.darcylf.me/image.js"></meta>
       <script
         async
         src="https://www.googletagmanager.com/gtag/js?id=UA-61981599-5"
