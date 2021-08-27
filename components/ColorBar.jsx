372ccfd83c387f09f78dc85ef302838342001480
Darcy LF <ehnevault@gmail.com>
2021-08-27 14:50:42 +1000

    fix the colour bar not changing

--- components/ColorBar.jsx
+++ components/ColorBar.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Box } from 'reflexbox';
 import { color } from '../lib/baseColor';
 
+import NoSSR from './NoSSR';
 
-const ColorBar = () => <Box
-    sx={{
-        width: '100%',
-        height: '0.2em',
-        backgroundImage: color('90deg'),
-    }}
-/>
-
+const ColorBar = () => (
+    <NoSSR>
+        <Box
+            sx={{
+                width: '100%',
+                height: '0.2em',
+                backgroundImage: color('90deg'),
+            }}
+        />
+    </NoSSR>
+)
 export default ColorBar;
