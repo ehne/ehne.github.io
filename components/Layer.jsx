6e76391c647beca6a72cb6938020952b58dd2bbe
Darcy LF <ehnevault@gmail.com>
2022-02-03 23:04:08 +1100

    :lipstick: use correct colours on layer

--- components/Layer.jsx
+++ components/Layer.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {m} from 'framer-motion';
 import Link from 'next/link';
-import { color } from '../lib/baseColor';
+import { color as baseColor } from '../lib/baseColor';
 
 
 const absoluteFill = {
@@ -23,15 +23,19 @@ const variants = {
 
 const Layer = ({children, isFirstLoad}) => {
   const duration = isFirstLoad ? 0 : .400 ;
+  const [color, setColor] = useState(`linear-gradient(90deg, #ffc53f 0%,#ffc53f 100%)`);
+    useEffect(()=> {
+        setColor(baseColor('45deg'));
+    }, []);
   return (
     <>
-      <Link href="/" passHref>
+      <Link href="/" passHref scroll={false}>
         <m.a
           style={{
             ...absoluteFill,
             opacity: 0.35,
             zIndex: 2,
-            backgroundImage: color('90deg'),
+            backgroundImage: color,
             filter: 'brightness(0.3)',
             display: 'block'
           }}
@@ -62,6 +66,7 @@ const Layer = ({children, isFirstLoad}) => {
           overflow: 'scroll',
           zIndex: 3,
           backgroundColor: '#fff',
+          borderLeft: `4px solid ${baseColor('90deg', 'color')}`
         }}
         
         variants={variants}
