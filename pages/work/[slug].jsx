55ae26b277e9ea2331dbc890aec2039f574d3b35
Darcy LF <ehnevault@gmail.com>
2022-02-03 20:14:18 +1100

    :sparkles: show content

--- pages/work/[slug].jsx
+++ pages/work/[slug].jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import { Box } from 'reflexbox';
+import Link from 'next/link';
+import Image from 'next/image'
 
 import IndexPage from '../index';
 import getPageData from '../../lib/getPageData';
 import { useRouter } from 'next/router';
+import { getAllPosts, getPostBySlug } from '../../lib/api';
+import ColorBar from '../../components/ColorBar';
 
-
-const WorkPage = ({posts, year}) => {
+const WorkPage = ({indexData, workData}) => {
   return (
     <>
-      <IndexPage posts={posts} year={year} />
-      <Box 
-        backgroundColor="black" 
-        opacity="0.5" 
-        sx={{
-          position: 'fixed',
-          top: 0,
-          left: 0, 
-          right: 0,
-          bottom: 0,
-        }}
-        as={'a'}
-        href="/"
-      />
+      <Box sx={{position: 'fixed', width: ['32rem', '34rem'], zIndex: 1}}>
+        <IndexPage {...indexData} />
+      </Box>
+      
+      <Link href="/">
+        <Box
+          backgroundColor="black"
+          opacity={0.3}
+          sx={{
+            position: 'fixed',
+            top: 0,
+            left: 0, 
+            right: 0,
+            bottom: 0,
+            zIndex: 2
+          }}
+        />
+      </Link>
       <Box
         sx={{
           position: 'fixed',
@@ -30,26 +37,51 @@ const WorkPage = ({posts, year}) => {
           top: 0,
           backgroundColor: '#fff',
           maxWidth: '30em',
-          padding: ['1em', '2em']
+          padding: ['1em', '2em'],
+          height: '100%',
+          overflow: 'scroll',
+          zIndex: 3
         }}
       >
-        hello
+        <Link href="/"><a>← go back</a></Link>
+        <Box as="section" my="2em">
+        <h2 style={{fontSize: '2em', fontStyle: 'italic', fontWeight: '700'}}>{workData.title}</h2>
+        <Box as="p">{workData.leadIn}</Box>
+        <Box as="img" my='1em' src={workData.image}/>
+        <ColorBar />
+        <p>{workData.content}</p>
+        </Box>
+        
       </Box>
     </>
   );
 }
 
-export const getStaticProps = async () => await getPageData();
+export const getStaticProps = async ({ params }) => {
+  const indexData = await getPageData();
+  const workData = getPostBySlug(params.slug, ["slug", "title", "leadIn", 'content', 'image']);
+  return {
+    props: {
+      indexData: indexData.props,
+      workData
+    }
+  }
+};
 
 export async function getStaticPaths() {
+  // onceagain, get the slugs
+  const allPosts = await getAllPosts(['slug']);
+
   return {
-    paths: [
-      // Object variant:
-      { params: { slug: 'Shareify' } },
-    ],
-    fallback: true,
-  }
+    paths: allPosts.map((post) => ({
+      params: {
+        slug: post.slug,
+      },
+    })),
+    fallback: false,
+  };
 }
+
 
 
 export default WorkPage;
