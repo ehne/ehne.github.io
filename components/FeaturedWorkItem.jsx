d5d2685f582c5153ad472e924a93602d664b0657
Darcy LF <7551957+ehne@users.noreply.github.com>
2023-04-06 18:34:16 +1000

    :sparkles: add loading indicator

--- components/FeaturedWorkItem.jsx
+++ components/FeaturedWorkItem.jsx
@@ -3,12 +3,18 @@ import { Box } from 'rebass';
 import Link from 'next/link';
 
 const FeaturedWorkItem = ({leadIn, title, slug}) => {
+    const [state, setState] = React.useState('idle');
+
     return (
         <Box
             marginBottom="1em"
             as="p"
         >
-            <b>{title}: </b> {leadIn} <Link scroll={false} href={`/work/${slug}`}><a>{'Read More →'}</a></Link>
+            <b>{title}: </b> {leadIn} <Link scroll={false} href={`/work/${slug}`} onClick={() => setState('loading')}>
+                <a onClick={() => setState('loading')} className={state === 'loading' ? 'link-loading' : ''}>
+                    {'Read More' + (state !== 'loading' ? ' →' : '')}
+                </a>
+            </Link>
         </Box>
     );
 }
