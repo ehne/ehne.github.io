f179c0ace5f8b64c8a60af7807d1c26179cd85ef
Darcy LF <7551957+ehne@users.noreply.github.com>
2023-04-16 23:35:23 +1000

    :fire: remove image fully

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true
-  },
-  integrations: [image()]
 });
