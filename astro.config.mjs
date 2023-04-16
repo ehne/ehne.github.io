bcc57c02a6945df39fcf15b81715620024d67d16
Darcy LF <7551957+ehne@users.noreply.github.com>
2023-04-16 19:50:33 +1000

    :seedling: add astro configs

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  integrations: [image()]
+});
